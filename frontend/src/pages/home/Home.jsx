3002a16e13eab0ad500d57dc1ec5bf81ef3dba72
Sanjit K. <skomputeruser@gmail.com>
2024-07-19 02:12:48 -0500

    started on frontend

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+        <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0 border'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default home
